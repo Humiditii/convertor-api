6a57a8bf53d5c8e4e59d56b5f4e3a50b32b7c689
Babatunde Abdulhameed Olakunle <humiditii45@gmail.com>
2020-06-16 20:54:28 +0000

    good status

--- controllers/ConvertController.js
+++ controllers/ConvertController.js
@@ -1,9 +1,42 @@
+import fs from 'fs';
+import xml2js from 'xml2js';
+import jsn from '../xml.json';
+
+
 class ConvertController {
-    static uploadFile(req, res, next){
+    static  uploadFile(req, res, next){
 
         const {file} = req;
 
-        console.log(file)
+        xml2js.parseString(file.buffer, (err, result)=> {
+            if(err){
+                throw err
+            }
+            const writejsn = JSON.stringify(result);
+        
+            fs.writeFileSync('xml.json', writejsn, (err, result) => {
+                if(err){
+                    throw err
+                }
+                console.log('Good')
+
+               //const jsn = require('../xml.json')
+             
+            })
+
+               
+            fs.writeFile('xml.txt',jsn.user.roles[0].role.map( (item, index) => (index+1 +' Role: '+item+'\n' +' name: '+jsn.user.name+'\n') ), (err, result) => {
+                if(err){
+                    throw err;
+                }
+                console.log('good')
+            })
+        })
+        
+
+        //console.log(x);
+
+       // console.log(file.buffer)
 
     }
 }
